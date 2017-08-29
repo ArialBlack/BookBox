ef3e9e7cc2d40f2b2ffb57163d8b78e0ae8541cb
ArialBlack <andreyturik@gmail.com>
2017-08-29 18:43:37 +0300

    add offline-page

---
+++ themes/bookbox_ui/templates/maintenance-page.tpl.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * @file
+ * Default theme implementation to display the basic html structure of a single
+ * Drupal page.
+ *
+ * Variables:
+ * - $css: An array of CSS files for the current page.
+ * - $language: (object) The language the site is being displayed in.
+ *   $language->language contains its textual representation.
+ *   $language->dir contains the language direction. It will either be 'ltr' or
+ *   'rtl'.
+ * - $html_attributes:  String of attributes for the html element. It can be
+ *   manipulated through the variable $html_attributes_array from preprocess
+ *   functions.
+ * - $html_attributes_array: An array of attribute values for the HTML element.
+ *   It is flattened into a string within the variable $html_attributes.
+ * - $body_attributes:  String of attributes for the BODY element. It can be
+ *   manipulated through the variable $body_attributes_array from preprocess
+ *   functions.
+ * - $body_attributes_array: An array of attribute values for the BODY element.
+ *   It is flattened into a string within the variable $body_attributes.
+ * - $rdf_namespaces: All the RDF namespace prefixes used in the HTML document.
+ * - $grddl_profile: A GRDDL profile allowing agents to extract the RDF data.
+ * - $head_title: A modified version of the page title, for use in the TITLE
+ *   tag.
+ * - $head_title_array: (array) An associative array containing the string parts
+ *   that were used to generate the $head_title variable, already prepared to be
+ *   output as TITLE tag. The key/value pairs may contain one or more of the
+ *   following, depending on conditions:
+ *   - title: The title of the current page, if any.
+ *   - name: The name of the site.
+ *   - slogan: The slogan of the site, if any, and if there is no title.
+ * - $head: Markup for the HEAD section (including meta tags, keyword tags, and
+ *   so on).
+ * - $styles: Style tags necessary to import all CSS files for the page.
+ * - $scripts: Script tags necessary to load the JavaScript files and settings
+ *   for the page.
+ * - $page_top: Initial markup from any modules that have altered the
+ *   page. This variable should always be output first, before all other dynamic
+ *   content.
+ * - $page: The rendered page content.
+ * - $page_bottom: Final closing markup from any modules that have altered the
+ *   page. This variable should always be output last, after all other dynamic
+ *   content.
+ * - $classes String of classes that can be used to style contextually through
+ *   CSS.
+ *
+ * @see bootstrap_preprocess_html()
+ * @see template_preprocess()
+ * @see template_preprocess_html()
+ * @see template_process()
+ *
+ * @ingroup templates
+ */
+?><!DOCTYPE html>
+<html<?php print $html_attributes;?><?php print $rdf_namespaces;?>>
+<head>
+    <link rel="profile" href="<?php print $grddl_profile; ?>" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php print $head; ?>
+    <title><?php print $head_title; ?></title>
+    <link type="text/css" rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" media="all">
+    <?php print $styles; ?>
+
+    <!-- HTML5 element support for IE6-8 -->
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <?php print $scripts; ?>
+    <script src="/sites/all/themes/bookbox_ui/js/jquery.nicescroll.min.js"></script>
+</head>
+<body class="offline">
+<div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+</div>
+
+<div class="main-container container text-center">
+    <div class="row">
+        <div class="col-sm-12">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </div>
+
+        <h2>Сайт вдосконалюється.</h2>
+        <p>До 8 вересня все знову працюватиме.</p>
+        <p>Дякуємо за розуміння.</p>
+
+    </div>
+</div>
+<span id="bpMArker"></span>
+<div class="fontdown hidden"><button class="btn btn-default btn-transparent"id="down">A -</button></div>
+<div class="fontup hidden"><button class="btn btn-default btn-transparent" id="up">A +</button></div>
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-96330036-1', 'auto');
+    ga('send', 'pageview');
+</script>
+</body>
+</html>
+
+
+
+
