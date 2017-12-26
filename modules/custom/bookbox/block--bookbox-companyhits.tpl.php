9b0441b303188a15b6177cd6b8ffa0b7af0c6db4
ArialBlack <andreyturik@gmail.com>
2017-12-26 21:48:15 +0200

    add blocks

---
+++ modules/custom/bookbox/block--bookbox-companyhits.tpl.php
@@ -0,0 +1,11 @@
+<?php if($bookslist !=null): ?>
+
+  <h2>Хіти в <?php print $name;?></h2>
+  <a href="/">Дивитись більше</a>
+
+  <?php print $bookslist; ?>
+
+<?php endif; ?>
+
+
+
---
+++ modules/custom/bookbox/block--bookbox-newbyadmin.tpl.php
@@ -0,0 +1,8 @@
+<?php if($bookslist !=null): ?>
+
+  <h2>Нові надходження</h2>
+  <a href="/">Дивитись більше</a>
+
+  <?php print $bookslist; ?>
+
+<?php endif; ?>
